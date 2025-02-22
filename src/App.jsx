0af5392defe93fda232014bb10bd59c093920ed8
web968 <web4069@gmail.com>
2025-02-22 11:47:54 +0300

    Cleaned

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,9 @@
-import React from "react";
-import { FaYoutube, FaTelegram, FaPhone } from "react-icons/fa";
+import React from 'react'
 
 export default function App() {
   return (
-    <>
-      <nav id="navbar">
-
-        <h1>Hello world!</h1>
-        {/* Navbar-top */}
-        <div className="nav-top">
-          <div className="container">
-          <div className="nav-content d-flex align-items-center justify-content-between">
-            <div className="left">
-              <ul className="d-flex align-items-center">
-                <li>
-                  <a href="#">Доставка</a>
-                </li>
-                <li>
-                  <a href="#">Контакты</a>
-                </li>
-                <li>
-                  <a href="#">О нас</a>
-                </li>
-              </ul>
-            </div>
-            <div className="right d-flex align-items-center">
-              <a href="tel:+998942534099">
-                <FaPhone className="call"/>
-                <span>+998942534099</span>
-              </a>
-              <div className="social-icons">
-              <a href="https://youtube.com" target="_blank">
-                <FaYoutube className="youtube"/>
-              </a>
-              <a href="https://t.me" target="_blank">
-                <FaTelegram className="telegram"/>
-              </a>
-              </div>
-            </div>
-          </div>
-          </div>
-          {/* Navbar-main */}
-          <div className="nav-bottom"></div>
-        </div>
-      </nav>
-    </>
-  );
+    <div>
+      <h1>Hello world!!!</h1>
+    </div>
+  )
 }
